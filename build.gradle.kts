836bcd506de5fd5108141afba679f7f30c8d32ea
ch8n <chetan.garg36@gmail.com>
2020-11-07 16:25:59 +0530

    added-versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.0-alpha15")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         // for proto-dataStore
         classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.12")
         // NOTE: Do not place your application dependencies here; they belong
